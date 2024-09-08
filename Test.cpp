120272d0d6f92ade83f07bfa2509bd753b9ca029
Adamyasood24 <soodadamya4@gmail.com>
2024-09-09 05:20:52 +0530

    test

---
+++ Test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void even_only(int a){
+    int sum =0;
+    int n=a;
+    for(int i=0;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            sum+=i;
+        }
+    }
+    cout<<"sum is =";
+    cout<<sum;
+}
+
+int main(){
+even_only(6'
+);
+
+
+}
